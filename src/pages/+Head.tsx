4333a87aafa588dbc3fc2509e3765685f230bb6e
Nguyen Duy <wuuyi123@gmail.com>
2025-12-17 00:59:13 +0700

    feat(font): use variable fonts from @fontsource

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -5,9 +5,10 @@ import previewImage from './previewImage.jpg'
 import iconMobile from './iconMobile.png'
 import conf from '$blog-config'
 
-import interRegularWoff2 from '../assets/fonts/Inter-Regular.woff2'
-import interSemiBoldWoff2 from '../assets/fonts/Inter-SemiBold.woff2'
-import lexendWoff2 from '../assets/fonts/Lexend.woff2'
+// import interLatin from '@fontsource-variable/inter/files/inter-latin-wght-normal.woff2'
+// import interVietnamese from '@fontsource-variable/inter/files/inter-vietnamese-wght-normal.woff2'
+// import lexendLatin from '@fontsource-variable/lexend/files/lexend-latin-wght-normal.woff2'
+// import lexendVietnamese from '@fontsource-variable/lexend/files/lexend-vietnamese-wght-normal.woff2'
 
 export default function Head() {
 
@@ -27,9 +28,10 @@ export default function Head() {
       <meta property="og:type" content="website" />
       <meta property="article:author" content="https://example.com/author" />
 
-      <link rel="preload" href={interRegularWoff2} as="font" type="font/woff2" crossorigin="anonymous" />
-      <link rel="preload" href={interSemiBoldWoff2} as="font" type="font/woff2" crossorigin="anonymous" />
-      <link rel="preload" href={lexendWoff2} as="font" type="font/woff2" crossorigin="anonymous" />
+      {/* <link rel="preload" href={interLatin} as="font" type="font/woff2" crossorigin="anonymous" />
+      <link rel="preload" href={interVietnamese} as="font" type="font/woff2" crossorigin="anonymous" />
+      <link rel="preload" href={lexendLatin} as="font" type="font/woff2" crossorigin="anonymous" />
+      <link rel="preload" href={lexendVietnamese} as="font" type="font/woff2" crossorigin="anonymous" /> */}
     </>
   )
 }
